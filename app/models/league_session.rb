d055f496f9cbd2989f819a78d648372a68452219
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-05 14:49:07 -0500

    Adds validations

--- app/models/league_session.rb
+++ app/models/league_session.rb
@@ -1,2 +1,5 @@
 class LeagueSession < ApplicationRecord
+  validates :gameone, numericality: { only_integer: true, in: 0..300 }, allow_blank: true
+  validates :gametwo, numericality: { only_integer: true, in: 0..300 }, allow_blank: true
+  validates :gamethree, numericality: { only_integer: true, in: 0..300 }, allow_blank: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get "/league_sessions" => "league_sessions#index"
-  post "/league_sessions" => "league_sessions#create"
-  get "/league_sessions/:id" => "league_sessions#show"
-  patch "/league_sessions/:id" => "league_sessions#update"
-  delete "/league_sessions/:id" => "league_sessions#destroy"
+  resources :league_sessions
+  # get "/league_sessions" => "league_sessions#index"
+  # post "/league_sessions" => "league_sessions#create"
+  # get "/league_sessions/:id" => "league_sessions#show"
+  # patch "/league_sessions/:id" => "league_sessions#update"
+  # delete "/league_sessions/:id" => "league_sessions#destroy"
 end
