ac802b30cea7b21e80284f170d12a8224cca5c27
Mike McComb <52894341+mikemccomb@users.noreply.github.com>
2023-07-27 15:55:11 -0500

    Updates CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173"
+    origins "example.com", "localhost:5173", "three9boards.render.com", "app39boards.render.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
